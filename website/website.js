08f54b4c2fef0386404f16bfe57bfa8374b61650
Shaharyar Ahmed <113702415+Shaharyar2310@users.noreply.github.com>
2023-05-30 21:52:54 +0400

    0
    000000

--- website/website.js
+++ website/website.js
@@ -1,13 +1,13 @@
-function opentab(evt, tab) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+function openCity(evt, cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
     }
-    document.getElementById(tab).style.display = "block";
-    evt.currentTarget.className += " active";
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " w3-red";
   }
